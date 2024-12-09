cc1c3963deeec326f60f64caba92b8757072d5ac
AxeTech <130759266+AxeTech@users.noreply.github.com>
2024-12-09 11:29:05 +0000

    This teaches us about Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+/* in the below example when run it will come as truye 
+but it shouldnt come as true as this is a string right ?
+should come as NaN but it comes as true as JS automatically
+converts string into a Number
+*/
+
+// DONT COMPARE TWO SEPRATE DATA TYPES 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+

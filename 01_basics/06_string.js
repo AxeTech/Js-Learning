70b05f1b605a3af6c6bcb68065c9080394cf8632
AxeTech <130759266+AxeTech@users.noreply.github.com>
2024-12-21 11:21:23 +0000

    Basics Of String

---
+++ 01_basics/06_string.js
@@ -0,0 +1,13 @@
+const name = "Kunal"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); --- This is an old way to do whcih is not very good way to do it 
+
+
+// This is a better way to write a string with variable -- This is called as String Interpolation
+// console.log(`Hello My Name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('KunalShah')
+
+console.log(gameName.length);
+console.log(gameName.charAt(3));
